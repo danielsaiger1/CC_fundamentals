129b909691bbb099cf811977cb3e53761c54a809
danielsaiger1 <daniel.saiger@yahoo.de>
2025-04-17 16:45:51 +0200

    flask backend for classification api

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from ultralytics import YOLO
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<p>Hello world</p>'
+
+@app.route('/classify', methods=['POST'])
+def hello_world():
+    model = YOLO('yolo11n-cls.pt')
+
+    img = request.files['image']
+
+    results = model(img)
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run()
