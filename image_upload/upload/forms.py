3a02e377e5ed4d62b9f3aa053aaccdf115e43eed
danielsaiger1 <daniel.saiger@yahoo.de>
2025-05-02 11:56:21 +0200

    updated frontend

--- image_upload/upload/forms.py
+++ image_upload/upload/forms.py
@@ -2,5 +2,4 @@ from django import forms
 
 
 class ImageUploadForm(forms.Form):
-    title = forms.CharField(max_length=50)
     image = forms.ImageField()
--- image_upload/upload/views.py
+++ image_upload/upload/views.py
@@ -8,16 +8,11 @@ from django.conf import settings
 import os
 import requests
 
-from ultralytics import YOLO
-
 from .forms import ImageUploadForm
-
-model = YOLO('yolo11n-cls.pt')
 
 def upload_image(request):
     form = ImageUploadForm()
     image_name = None
-    prediction_result = None
     prediction_result_flask = None
     url = 'http://localhost:5000/classify'
 
@@ -32,17 +27,10 @@ def upload_image(request):
             for chunk in image.chunks():
                 f.write(chunk)
 
-        results = model(image_path)
         with open(image_path, 'rb') as img_file:
             files = {'image': img_file}
             flask_response = requests.post(url, files=files)
             prediction_result_flask = flask_response.json()
             print(f"RESULT: {prediction_result_flask}")
 
-        for result in results:
-            prediction_result = {
-                'class' : f"{result.names[result.probs.top1]}",
-                'prob' : f"{result.probs.top1conf:.2f}"
-            }        
-
-    return render(request, 'upload/upload_image.html', {'image_path': image_name, 'form': form, 'prediction_result': prediction_result, 'flask_response': prediction_result_flask})
+    return render(request, 'upload/upload_image.html', {'image_path': image_name, 'form': form, 'flask_response': prediction_result_flask})
