e6f9d30c7a4d2d431469ae948115c266814aec16
danielsaiger1 <daniel.saiger@yahoo.de>
2025-05-22 12:45:44 +0200

    updated url for classification API

--- frontend/app.py
+++ frontend/app.py
@@ -10,8 +10,8 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     image_name = None
-    prediction_result_flask = None
-    flask_classifier_url = 'http://localhost:5000/classify' 
+    prediction_result = None
+    classifier_url = 'https://cc-fund-classification-api.azurewebsites.net/classify'
 
     if request.method == 'POST':
         if 'image' not in request.files:
@@ -28,13 +28,13 @@ def upload_image():
 
             with open(image_path, 'rb') as img_file:
                 files = {'image': img_file}
-                response = requests.post(flask_classifier_url, files=files)
-                prediction_result_flask = response.json()
-                print("RESULT:", prediction_result_flask)
+                response = requests.post(classifier_url, files=files)
+                prediction_result = response.json()
+                print("RESULT:", prediction_result)
 
             image_name = filename
 
-    return render_template('index.html', image_path=image_name, flask_response=prediction_result_flask)
+    return render_template('index.html', image_path=image_name, response=prediction_result)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=8080)
