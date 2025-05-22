fcf51652bfc8d8a6ecbaa20d502ff341d59d3b9f
danielsaiger1 <daniel.saiger@yahoo.de>
2025-05-22 11:07:06 +0200

    added terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "cc-fund-rg"
+  location = "North Europe"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "cc-fund-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+  reserved            = true
+}
+
+resource "azurerm_linux_web_app" "frontend-app" {
+  name                = "cc-fund-frontend-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+  client_certificate_mode = "Required"
+  public_network_access_enabled = true
+
+  site_config {
+    linux_fx_version          = "PYTHON|3.13"
+    always_on                 = false
+    http2_enabled             = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_linux_web_app" "classification-app" {
+  name                = "cc-fund-classification-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+  client_certificate_mode = "Required"
+  public_network_access_enabled = true
+
+  site_config {
+    linux_fx_version          = "PYTHON|3.11"
+    always_on                 = false
+    http2_enabled             = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "subscription_id" {
+  type        = string
+  description = "Subscription ID for the project"
+  sensitive = true
+}
